d7ea5430464e557008e7d753d739319020482f00
cookapps-jhlee11 <jhlee11@cookapps.com>
2022-08-18 18:31:48 +0900

    feat: S3버킷 주소 및 json파일 액세스 불가능하도록 수정

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    config: [
+        {
+            "url": "http://tech-swagger-test.s3-website.ap-northeast-2.amazonaws.com/api-docs/swagger-spec-nestjs-boilerplate.json",
+            "name": "nestjs-boilerplate-app-api"
+        },
+        {
+            "url": "https://petstore.swagger.io/v2/swagger.json",
+            "name": "Swagger-official-example-api"
+        },
+    ]
+}
--- src/swagger-initializer.js
+++ src/swagger-initializer.js
@@ -1,14 +1,12 @@
+const config = require("./config/config");
+
 window.onload = function() {
     //<editor-fold desc="Changeable Configuration Block">
 
     // the following lines will be replaced by docker/configurator, when it runs in a docker-container
     window.ui = SwaggerUIBundle({
         //url: "https://petstore.swagger.io/v2/swagger.json",
-        //url: "http://tech-swagger-test.s3-website.ap-northeast-2.amazonaws.com/swagger/swagger-spec.json",
-        urls:[
-            {"url":"http://tech-swagger-test.s3-website.ap-northeast-2.amazonaws.com/swagger/swagger-spec.json","name":"nestjs-boilerplate-app-api"},
-            {"url":"https://petstore.swagger.io/v2/swagger.json","name":"Swagger-official-example-api"},
-        ],
+        urls: config,
         dom_id: '#swagger-ui',
         deepLinking: true,
         presets: [
