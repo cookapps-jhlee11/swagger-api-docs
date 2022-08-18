422fd935604359b915a7365cd83238d5a5a08ed2
cookapps-jhlee11 <jhlee11@cookapps.com>
2022-08-18 18:03:08 +0900

    feat: Migrated swagger-ui-dist files from nestjs-boilerplate-app.git

---
+++ swagger-ui-dist/swagger-initializer.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    //<editor-fold desc="Changeable Configuration Block">
+
+    // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+    window.ui = SwaggerUIBundle({
+        //url: "https://petstore.swagger.io/v2/swagger.json",
+        //url: "http://tech-swagger-test.s3-website.ap-northeast-2.amazonaws.com/swagger/swagger-spec.json",
+        urls:[
+            {"url":"http://tech-swagger-test.s3-website.ap-northeast-2.amazonaws.com/swagger/swagger-spec.json","name":"nestjs-boilerplate-api"},
+            {"url":"https://petstore.swagger.io/v2/swagger.json","name":"Swagger-official-example-api"},
+        ],
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout"
+    });
+
+    //</editor-fold>
+};
